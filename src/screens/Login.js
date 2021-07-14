1754f40f3d604175c1df3a9218d6fa7032e461a9
Eugemo <mariaeugeniamoreno@hotmail.com>
2021-07-13 21:01:01 -0300

    add login con google

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,105 +1,150 @@
-import React,{ Component } from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   Dimensions,
-  StatusBar,
   StyleSheet,
   Text,
   ImageBackground,
   TouchableOpacity,
   View,
-  Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button } from 'react-native-elements';
-//import { Button } from 'react-native-paper';
+import {Input, Button} from 'react-native-elements';
+import {connect} from 'react-redux';
+import {actions} from '../store';
+import auth from '@react-native-firebase/auth';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const height = Dimensions.get('window').height
-const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
 
+GoogleSignin.configure({
+  webClientId: '302610177364-un4cokhmdj0smiarspna181o2og0uh56.apps.googleusercontent.com',
+});
 
-export default class Log extends React.Component {
+class Login extends React.Component {
+  
 
-  _onLogPress = () => {
-    Alert.alert(
-      "Hola",
-      "Ya te encuentras en Login",
-      [
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
-    );
-  }
+  onGoogleButtonPress = async () => {
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  };
 
-
-  render(){
-    return( 
-      <SafeAreaView style={{flex:1}}>
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
         <View style={styles.content}>
-            <ImageBackground
-                style={{width, height}} 
-                source={require('../assets/images/sakurafondo.jpg')}
-            >
+          <ImageBackground
+            style={{width, height}}
+            source={require('../assets/images/sakurafondo.jpg')}>
             <Text style={styles.text}> Login </Text>
-            <Input style={styles.input}
-            placeholder='Ingrese Usuario'
-            leftIcon={<Icon 
-                name='user'
-                size={24}
-                color='#512E5F'/>}
+            <Input
+              style={styles.input}
+              placeholder="Ingrese Usuario"
+              leftIcon={<Icon name="user" size={24} color="#512E5F" />}
             />
 
-            <Input style={styles.input} 
-            placeholder="Password" 
-            secureTextEntry={true} 
-            leftIcon={<Icon 
-                name='lock'
-                size={24}
-                color='#512E5F'/>}
+            <Input
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry={true}
+              leftIcon={<Icon name="lock" size={24} color="#512E5F" />}
             />
 
-            <TouchableOpacity style={[styles.button, { backgroundColor:'rgba(165, 105, 189, 0.5)' }]}>
-              <Text style={styles.text}>
-                Aceptar
-              </Text>
-            </TouchableOpacity>  
+            <TouchableOpacity
+              style={[
+                styles.button,
+                {backgroundColor: 'rgba(165, 105, 189, 0.5)'},
+              ]}>
+              <Text style={styles.text}>Aceptar</Text>
+            </TouchableOpacity>
             <Text style={styles.text}>
-                <Button title='Continuar con Google' />
-              </Text> 
-              
-            </ImageBackground>
+              {/* <Button title='Continuar con Google'
+                onPress={()=>this.onGoogleButtonPress().then(async(data)=>{
+                  console.log('Signed in with Google!')
+                  if(data){
+                  console.log('res login: '+JSON.stringify(data.user))
+                  try {
+                  await AsyncStorage.setItem('isloged', JSON.stringify(data.user))
+                  } catch (e) {
+                  console.log('Hubo un error :'+e)
+                  }
+                  this.props.setUser(data.user)
+                  }
+                  })} /> */}
+              <GoogleSigninButton
+                style={{width: 192, height: 48}}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Ligth}
+                onPress={() =>
+                  this.onGoogleButtonPress()
+                  .then(async data => {
+                    console.log('Signed in with Google!');
+                    if (data) {
+                      console.log('res login: ' + JSON.stringify(data.user));
+                      try {
+                        await AsyncStorage.setItem(
+                          'isloged',
+                          JSON.stringify(data.user),
+                        );
+                      } catch (e) {
+                        console.log('Hubo un error :' + e);
+                      }
+                      this.props.setUser(data.user);
+                    }
+                  }).catch (err => {console.log(err)})
+                }
+              />
+            </Text>
+          </ImageBackground>
         </View>
       </SafeAreaView>
-     
-    )}
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    content:{
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    text: {
-      fontSize:30, 
-      fontWeight:'bold', 
-      color:'#512E5F',
-      textAlign:'center',
-      marginTop: 10,
-    },
-    input: {
-        fontSize:20, 
-        fontWeight:'bold', 
-        color:'#512E5F',
-        textAlign:'center',
-        backgroundColor:'#F4ECF7',
-        margin: width/20,
-    },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#512E5F',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  input: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#512E5F',
+    textAlign: 'center',
+    backgroundColor: '#F4ECF7',
+    margin: width / 20,
+  },
 
-      button: {
-        margin: width/10,
-        borderRadius:35,
-        justifyContent:'center',
-        
-      }
-  })
+  button: {
+    margin: width / 10,
+    borderRadius: 35,
+    justifyContent: 'center',
+  },
+});
+
+const mapDispatchToProps = dispatch => ({
+  setUser: data => dispatch(actions.user.setUser(data)),
+});
+const mapStateToProps = state => ({
+  user: state.user.user,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,44 +1,93 @@
-import React,{ Component } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  Dimensions,
-  StatusBar,
-  StyleSheet,
-  Text,
-  ImageBackground,
-  TouchableOpacity,
-  View,
-  Alert
-} from 'react-native';
+import React, {Component} from 'react';
+import {SafeAreaView, Dimensions, StyleSheet, Text, View} from 'react-native';
+import {connect} from 'react-redux';
+import {Avatar, Button} from 'react-native-elements';
+import auth from '@react-native-firebase/auth';
+import {actions} from '../store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const height = Dimensions.get('window').height
-const width = Dimensions.get('window').width
-
-
-export default class Profile extends React.Component {
-
-  _onProfilePress = () => {
-    Alert.alert(
-      "Hola",
-      "Ya te encuentras en Profile",
-      [
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
+const {height, width} = Dimensions.get('window');
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      photoURL: '',
+      name: '',
+    };
+  }
+  componentDidMount = () => {
+    const {user} = this.props;
+    console.log('user profile: ' + JSON.stringify(user));
+    this.setState({
+      email: user.providerData[0].email,
+      photoURL: user.providerData[0].photoURL,
+      name: user.providerData[0].displayName,
+    });
+  };
+  render() {
+    const {email, photoURL, name} = this.state;
+    return (
+      <SafeAreaView
+        style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.content}>
+          <View style={{alignItems: 'center'}}>
+            <Avatar rounded source={{uri: photoURL}} size="xlarge" />
+            <View style={styles.dataContainer}>
+              <Text style={styles.infoText}>{email}</Text>
+              <Text style={styles.infoText}>{name}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{flex: 1, top: 50, width: width * 0.5}}>
+          <Button
+            title="Salir"
+            onPress={() => {
+              auth()
+                .signOut()
+                .then(async () => {
+                  console.log('User signed out!'),
+                    this.props.setUser({user: null});
+                  try {
+                    await AsyncStorage.delItem('isloged');
+                  } catch (e) {
+                    console.log('Hubo un error :' + e);
+                  }
+                });
+            }}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
-
-
-  render(){
-    return( 
-      <SafeAreaView style={{flex:1}}>
-        <ImageBackground
-          style={{height}}
-          source={require('../assets/images/sakurafondo.jpg')}
-        >
-          <Text>Profile</Text>
-        </ImageBackground>
-    </SafeAreaView>  
-     
-    )}
 }
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    // color:'#fff',
+    textAlign: 'center',
+  },
+  content: {
+    flex: 1,
+    top: 50,
+    justifyContent: 'center',
+    // alignItems:'center'
+  },
+  dataContainer: {
+    top: 50,
+    width,
+  },
+  infoText: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: 'grey',
+  },
+});
+const mapDispatchToProps = dispatch => ({
+  setUser: ({user}) => dispatch(actions.user.setUser({user})),
+});
+const mapStateToProps = state => ({
+  user: state.user.user,
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
